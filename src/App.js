50e6fa092b57821a3eae5efe7e473ecaf5db725d
Katarína Semanová <katarina.semanova@student.tuke.sk>
2025-05-01 21:17:38 +0200

    under construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
+
+      <main className='App-main'>
+        <b>
+        Zvedaví, kde budete sedieť?
+        </b>
+        <div>
+        Zasadací poriadok bude dostupný <b className="App-main-date">16. 10. 2025</b> – deň pred našou svadbou.
+        </div>
+        <p>
+        Nezabudnite sa sem vrátiť, aby ste zistili, pri kom budete stolovať a kde vás čaká vaše miesto pri stole.
+        </p>
+      </main>
     </div>
   );
 }
